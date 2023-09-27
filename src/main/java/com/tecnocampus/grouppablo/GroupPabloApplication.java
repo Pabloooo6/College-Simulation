fde4f0ad5a86fef1b4e4a6fe2bde07e75c3e7b2a
Pabloooo6 <pschneegluth@edu.tecnocampus.cat>
2023-09-27 08:28:02 +0200

    first push

---
+++ src/main/java/com/tecnocampus/grouppablo/GroupPabloApplication.java
@@ -0,0 +1,13 @@
+package com.tecnocampus.grouppablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupPabloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupPabloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tecnocampus/grouppablo/GroupPabloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnocampus.grouppablo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupPabloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
